adb3ca9a6cdffc91f036c6ad2497d9649955fd3f
zohaibmemon1515 <zohaibmemon1515@gmail.com>
2025-07-20 12:59:07 +0500

    add files

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://rhistae-frontend.vercel.app/"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
